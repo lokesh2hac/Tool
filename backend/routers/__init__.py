0d5ec1518ee55a72f6d6d0b079589e3dacb4801b
lokesh2hac <l24819736@gmail.com>
2026-06-28 12:52:56 +0530

    Update __init__.py

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -1,8 +1,1 @@
-from . import auth
-from . import groups
-from . import candidates
-from . import outreach
-from . import api_keys
-from . import auto_scan
-from . import sessions
-from . import group_posts  # 👈 add this
+
